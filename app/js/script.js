1e995a4750f6e1861e6df1a0bef7f6b85819caee
Bliss Perry <blissperry123@gmail.com>
2018-01-17 17:04:13 -0500

    Added form to landing page

--- app/js/script.js
+++ app/js/script.js
@@ -266,5 +266,15 @@ function loadRestaurant(id) {
   restUrlDiv.appendChild(restUrl)
   parent.appendChild(restUrlDiv)
 
+}
 
+function makeAppointment(restId) {
+  console.log('hi')
+
+  var form = document.forms[0]
+
+  console.log(restId)
+
+  var appointment_time = form.appointment_time.value
+  console.log(appointment_time)
 }
